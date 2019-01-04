6505446d2f370781fe11370589a9bc9abd934477
patw <patw.hi@gmail.com>
2019-01-04 17:26:31 +0800

    Updates

---
+++ precache-manifest.927cefd975d9a47f0e953964970ab027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15db3d47431cfff73673",
+    "url": "/static/js/main.15db3d47.chunk.js"
+  },
+  {
+    "revision": "2baff26803cec17d3d81",
+    "url": "/static/js/1.2baff268.chunk.js"
+  },
+  {
+    "revision": "15db3d47431cfff73673",
+    "url": "/static/css/main.478092f6.chunk.css"
+  },
+  {
+    "revision": "a699200d82a77fb42bfeb17fd26561a6",
+    "url": "/index.html"
+  }
+];
