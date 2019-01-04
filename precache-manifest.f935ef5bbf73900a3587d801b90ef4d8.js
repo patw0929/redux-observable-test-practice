d4e9ca38047d13dba2ddbcc8d496dec1975f01d8
patw <patw.hi@gmail.com>
2019-01-04 17:34:39 +0800

    Updates

---
+++ precache-manifest.f935ef5bbf73900a3587d801b90ef4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7fd962d3976df2223ab",
+    "url": "/redux-observable-test-practice/static/js/runtime~main.c7fd962d.js"
+  },
+  {
+    "revision": "5b10c6b58f28160ebcaa",
+    "url": "/redux-observable-test-practice/static/js/main.5b10c6b5.chunk.js"
+  },
+  {
+    "revision": "2baff26803cec17d3d81",
+    "url": "/redux-observable-test-practice/static/js/1.2baff268.chunk.js"
+  },
+  {
+    "revision": "5b10c6b58f28160ebcaa",
+    "url": "/redux-observable-test-practice/static/css/main.478092f6.chunk.css"
+  },
+  {
+    "revision": "c8af6f7abbf72fc3c3769052382968a0",
+    "url": "/redux-observable-test-practice/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.927cefd975d9a47f0e953964970ab027.js"
+  "/redux-observable-test-practice/precache-manifest.f935ef5bbf73900a3587d801b90ef4d8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/redux-observable-test-practice/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
