9fca7da624e99107d0e64751a54f45861f4a63b4
patw <patw.hi@gmail.com>
2019-01-04 10:35:33 +0800

    Put search components into main screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-import logo from './logo.svg';
 import './App.css';
+import SearchBox from './SearchBox/SearchBoxContainer';
+import SearchResult from './SearchResult/SearchResultContainer';
 
 const store = configureStore({});
 
@@ -10,21 +11,9 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+        <div className="container-fluid">
+          <SearchBox />
+          <SearchResult />
         </div>
       </Provider>
     );
